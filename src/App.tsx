ac8458733020c7c0001b859528b3ebe5cec54197
Anand Raj Singh <singhpariharanandraj@gmail.com>
2023-08-14 11:47:01 +0530

    Home Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Navbar from '@/scenes/navbar'
+import Home from './scenes/home';
 import { SelectedPage } from '@/shared/types';
 
 function App() {
@@ -20,7 +21,8 @@ function App() {
 
   return (
     <div className='app bg-gray-20'>
-      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   )
 }
