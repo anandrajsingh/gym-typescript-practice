eb9f3dd0d518818ff4e05e056de7de35f168099f
Anand Raj Singh <singhpariharanandraj@gmail.com>
2023-08-14 18:28:38 +0530

    Our Classes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/scenes/navbar'
 import Home from './scenes/home';
 import { SelectedPage } from '@/shared/types';
 import Benefits from '@/scenes/benefits';
+import OurClasses from './scenes/ourClasses';
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
@@ -25,6 +26,7 @@ function App() {
       <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage}/>
+      <OurClasses setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
